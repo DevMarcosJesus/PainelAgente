b930ff5d10f259adc9ed21b7f8b18dc831779268
Marcos Jesus <marcos@evence.com.br>
2021-06-21 16:23:12 -0300

    Up painel

---
+++ Agentes/resources/assets/js/agentes/components/Tela.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h3>This Vue running in component together blade.</h3>
+      <p>it's okay.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ Agentes/resources/views/vue.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>VueJS</title>
+</head>
+<body>
+    <h2>uhdasuh</h2>
+    <tela-component></tela-component>
+
+
+    <script src="{{ mix('js/agentes/app.js') }}">
+ 
+ 
+</body>
+</html>
